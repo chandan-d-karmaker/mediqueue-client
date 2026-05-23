89795ab09ceb3f02f772986934b1d88750021590
Chandan D Karmaker <chandankarmaker555@gmail.com>
2026-05-24 00:53:04 +0600

    feat: add new image remote pattern and update Hero component structure

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,12 @@ const nextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'img.daisyui.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
--- src/app/components/main/Hero.jsx
+++ src/app/components/main/Hero.jsx
@@ -2,12 +2,21 @@ import Image from 'next/image';
 import React from 'react';
 import footerBg from '@/assests/WaveLine.svg'
 {/* <Image src={footerBg} alt='test' height={100} width={100} className='w-full'></Image> */ }
+{/* <Image
+    src="https://img.daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.webp" alt='slider1' width={300} height={200}
+    className="w-full" /> */}
 
 const Hero = () => {
     return (
-        <>
-            
-        </>
+        <div id="slide1" className="carousel-item relative w-full">
+            <Image
+                src="https://img.daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.webp" alt='slider1' width={300} height={200}
+                className="w-full" />
+            <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+                <a href="#slide4" className="btn btn-circle">❮</a>
+                <a href="#slide2" className="btn btn-circle">❯</a>
+            </div>
+        </div>
 
     );
 };
