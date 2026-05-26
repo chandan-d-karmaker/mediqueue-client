e84f8b5a7f8ea05696e0185a9f68a6b042665643
Chandan D Karmaker <chandankarmaker555@gmail.com>
2026-05-26 20:31:36 +0600

    feat: add WhyChoose component to Home page and enable smooth scrolling in layout

--- src/app/(main)/page.js
+++ src/app/(main)/page.js
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import Hero from "../components/main/Hero";
 import AvailableTutors from "../components/main/AvailableTutors";
+import WhyChoose from "../components/main/WhyChoose";
 
 export default function Home() {
   return (
     <div className="space-y-10">
       <Hero/>
       <AvailableTutors/>
+      <WhyChoose/>
       
     </div>
   );
--- src/app/layout.js
+++ src/app/layout.js
@@ -20,7 +20,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html
-      lang="en" data-theme="light"
+      lang="en" data-theme="light" scroll-behavior="smooth"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className=" flex flex-col">
