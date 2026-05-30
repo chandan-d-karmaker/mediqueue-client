aacd74b055f45bd612ff0f04b601c8d536964e13
Chandan D Karmaker <chandankarmaker555@gmail.com>
2026-05-30 13:24:06 +0600

    feat: add ThemeProvider for theme management and update NavBar and WhyChoose components for dark mode support

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "react-hot-toast";
+import ThemeProviders from "./providers/ThemeProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -20,14 +21,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html
-      lang="en" data-theme="light" scroll-behavior="smooth"
+      lang="en" scroll-behavior="smooth" suppressHydrationWarning
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className=" flex flex-col">
-
-        <main>
-          {children}
-        </main>
+        <ThemeProviders>
+          <main>{children}</main>
+        </ThemeProviders>
         <Toaster />
       </body>
     </html>
