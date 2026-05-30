94c1b5251edc82a458cb55cf2d37f842539fd70d
Chandan D Karmaker <chandankarmaker555@gmail.com>
2026-05-30 14:01:46 +0600

    feat: tring to show users added tutors. Faced error and trying to fix

--- src/app/(main)/my-tutors/page.jsx
+++ src/app/(main)/my-tutors/page.jsx
@@ -1,17 +1,22 @@
-'use client'
-import { useSession } from '@/lib/auth-client';
+// import { auth } from '@/lib/auth';
+// import { headers } from 'next/headers';
 import React from 'react';
 
-const MyTutorsPage = () => {
+const MyTutorsPage = async () => {
 
-    const { data: session } = useSession();
-    const { id } = session?.user;
-    // console.log(user);
+    // const session = await auth.api.getSession({
+    //     headers: await headers(), // you need to pass the headers object.
+    // });
+    // const { id } = session?.user;
+    // console.log(id);
 
-    
+    // const res = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/all-tutors/${id}`)
+    // const myTutors = await res.json();
+    // console.log(myTutors);
+
     return (
         <div>
-            
+            my added tutors be here
         </div>
     );
 };
