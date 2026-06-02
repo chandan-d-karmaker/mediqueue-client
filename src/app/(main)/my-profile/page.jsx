3aec08c14f7c33bf4fe47ed6dc7e15e8e77f45d6
Chandan D Karmaker <chandankarmaker555@gmail.com>
2026-06-02 14:34:13 +0600

    feat: implement profile page with user information display and update functionality

---
+++ src/app/(main)/my-profile/page.jsx
@@ -0,0 +1,55 @@
+import UpdateProfileModal from '@/app/components/shared/UpdateProfileModal';
+import { auth } from '@/lib/auth';
+import { title } from 'motion/react-client';
+import { headers } from 'next/headers';
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+
+export const metadata = {
+    title: "My Profile",
+};
+
+const ProfilePage = async () => {
+
+    const session = await auth.api.getSession({
+        headers: await headers(), // you need to pass the headers object.
+    });
+    // console.log(session);
+    const user = session?.user;
+    console.log(user);
+
+
+    // add a better loading state later
+    if (!user) {
+        return <div className='flex justify-center items-center'>
+            <span className="loading loading-ring loading-xl"></span>
+        </div>;
+    }
+    return (
+        <div className='animate__animated animate__backInDown my-10'>
+            {
+                user ? <div className='flex flex-col items-center justify-center space-y-4 text-center bg-linear-to-tl from-gray-900 via-blue-300 to-gray-900 md:w-1/2 md:mx-auto p-8 rounded-2xl mx-4'>
+
+                    <Image
+                        src={user?.image}
+                        alt={user?.name}
+                        width={100}
+                        height={100}
+                        className='rounded-full w-25 h-25'
+                    />
+                    <p className='font-medium text-black'>Name: {user?.name}</p>
+                    <p className='font-medium text-black'>Email: {user?.email}</p>
+
+                    <div>
+                        <UpdateProfileModal/>
+                    </div>
+                </div> : <div className=' flex items-center justify-center h-screen'>
+                    <h2 className='text-2xl rounded-2xl font-medium bg-base-200 p-10 text-center'>Please login first</h2>
+                </div>
+            }
+        </div>
+    );
+};
+
+export default ProfilePage;
---
+++ src/app/components/shared/UpdateProfileModal.jsx
@@ -0,0 +1,57 @@
+'use client'
+import React from 'react';
+import { authClient } from "@/lib/auth-client";
+
+const UpdateProfileModal = () => {
+
+    const handleUpdate = async (e) => {
+        e.preventDefault();
+        const formData = new FormData(e.target);
+        const userData = Object.fromEntries(formData.entries());
+
+        const { data, error } = await authClient.updateUser({
+            ...userData
+        });
+
+        if (!error) {
+            document.getElementById('update_profile_modal').close();
+            window.location.reload();
+        }
+    };
+
+    return (
+        <div>
+            <button
+                className="btn btn-primary"
+                onClick={() => document.getElementById('update_profile_modal').showModal()}
+            >
+                Edit Profile
+            </button>
+            <dialog id="update_profile_modal" className="modal modal-bottom sm:modal-middle">
+                <div className="modal-box p-0 overflow-hidden">
+                    <form onSubmit={handleUpdate} className="fieldset bg-base-100 p-6">
+                        <legend className="fieldset-legend text-lg font-bold">Update Information</legend>
+
+                        <label className="label">Name</label>
+                        <input name='name' type="text" className="input w-full" placeholder="Name" />
+
+                        <label className="label">Photo URL</label>
+                        <input name='image' type="text" className="input w-full" placeholder="photo url" />
+
+                        <div className="modal-action">
+                            <button type="button" className="btn btn-ghost" onClick={() => document.getElementById('update_profile_modal').close()}>Cancel</button>
+                            <button type="submit" className="btn btn-neutral">Update info</button>
+                        </div>
+                    </form>
+                </div>
+                {/* Backdrop to close modal when clicking outside */}
+                <form method="dialog" className="modal-backdrop">
+                    <button>close</button>
+                </form>
+            </dialog>
+        </div>
+
+    );
+};
+
+export default UpdateProfileModal;
