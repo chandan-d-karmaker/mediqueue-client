1e47cef99025fb238bcc51be764ba4e6ff7b51a4
Chandan D Karmaker <chandankarmaker555@gmail.com>
2026-06-04 21:36:45 +0600

    feat: format session start date in FeatTutorCard for improved readability

--- src/app/components/shared/FeatTutorCard.jsx
+++ src/app/components/shared/FeatTutorCard.jsx
@@ -3,13 +3,17 @@ import Link from 'next/link'
 import React from 'react';
 
 const FeatTutorCard = async ({ tutor }) => {
-    
+
+    const isoString = tutor.sessionStartDate;
+
+    const formattedDate = format(new Date(isoString), 'd MMM yyyy').toUpperCase();
+
     return (
         <div className="rounded-lg p-4 space-y-1  bg-base-100 shadow-sm border-2 border-base-300 transition-all duration-300 hover:shadow-[8px_8px_0px_0px_#555] hover:-translate-y-1 hover:-translate-x-1">
             <Image src={tutor.imageUrl} alt={tutor.name} width={100} height={100} className='w-full h-50 rounded-lg border'></Image>
             <h1 className='text-xl font-semibold'>{tutor.name}</h1>
             <p className='text-md opacity-75'>{tutor.subject}</p>
-            <p className='text-md'>Season Start Date: {tutor.sessionStartDate} </p>
+            <p className='text-md'>Season Start Date: {formattedDate} </p>
             <p className='text-xl font-semibold'>Fee: {tutor.hourlyFee}Tk/hr</p>
             <Link href={`/all-tutors/${tutor._id}`} className="btn btn-primary w-full">
                 Book Session
